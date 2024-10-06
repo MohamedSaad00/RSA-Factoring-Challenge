f2b17df56d57eb7a339450cc77d54aae907eba3f
MohamedSaad00 <mohamed.saadx4@gmail.com>
2024-10-06 12:21:15 -0700

    Factorize all the things!

--- factors.c
+++ factors.c
@@ -1,45 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Function to find two factors of a number */
-void factorize(unsigned long long int n) {
+/**
+ * factorize - Finds two factors of a number and prints them
+ * @n: The number to be factored
+ */
+void factorize(unsigned long long int n)
+{
     unsigned long long int p, q;
 
-    for (p = 2; p * p <= n; p++) {
-        if (n % p == 0) {
+    if (n % 2 == 0)
+    {
+        q = n / 2;
+        printf("%llu=%llu*2\n", n, q);
+        return;
+    }
+
+    for (p = 3; p * p <= n; p += 2)
+    {
+        if (n % p == 0)
+        {
             q = n / p;
             printf("%llu=%llu*%llu\n", n, q, p);
             return;
         }
     }
-    /* If no factors found, it must be a prime number */
+
+    /* If no factors are found, print n=1*n */
     printf("%llu=%llu*1\n", n, n);
 }
 
-int main(int argc, char *argv[]) {
+/**
+ * main - Reads numbers from a file and factors them
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
     FILE *file;
     char *line = NULL;
     size_t len = 0;
     unsigned long long int n;
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return 1;
+        return (1);
     }
 
     file = fopen(argv[1], "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Error opening file");
-        return 1;
+        return (1);
     }
 
     /* Read each line and factorize the number */
-    while (getline(&line, &len, file) != -1) {
+    while (getline(&line, &len, file) != -1)
+    {
         n = strtoull(line, NULL, 10);
         factorize(n);
     }
 
     free(line);
     fclose(file);
-    return 0;
+    return (0);
 }
